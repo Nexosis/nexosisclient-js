a085732c1af18e075df3410fcff352b5e212469d
Zach Bussey <zbussey@nexosis.com>
2017-12-08 17:36:35 -0500

    Adding more test cases.

--- tests/session.test.js
+++ tests/session.test.js
@@ -13,6 +13,8 @@ import DataSetClient from '../src/DataSetClient';
 describe('Session tests', () => {
     var client = new SessionClient({ endpoint: global.endpointUrl, key: process.env.NEXOSIS_API_TESTKEY });
     var dataClient = new DataSetClient({ endpoint: global.endpointUrl, key: process.env.NEXOSIS_API_TESTKEY });
+    let forecastSessionId;
+
     before(function (done) {
         dataClient.create("TestNode", { "data": [{ "timestamp": "1-1-2017", "sales": 135.32 }, { "timestamp": "1-2-2017", "sales": 235.31 }, { "timestamp": "1-3-2017", "sales": 335.42 }, { "timestamp": "1-04-2017", "sales": 65.98 }, { "timestamp": "1-05-2017", "sales": 255.23 }] })
             .then(response => { })
@@ -23,23 +25,37 @@ describe('Session tests', () => {
         dataClient.remove("TestNode").then(() => done()).catch(err => done(err));
     });
 
-    it('can create an impact session', (done) => {
-        var result = client.analyzeImpact("TestNode", '1-03-2017', '1-05-2017', 'myevent', 'sales');
-        expect(result).to.be.a('Promise');
-        result.then((data) => {
-            expect(data.dataSetName).to.equal('TestNode');
-        }).then(done)
-            .catch((err) => { done(err); });
-    });
+    it('can create an impact session', mochaAsync(async () => {
+        var result = await client.analyzeImpact("TestNode", '1-03-2017', '1-05-2017', 'myevent', 'sales');
+        expect(result.dataSetName).to.equal('TestNode');
+    }));
 
-    it('can create an forecast session', (done) => {
-        var result = client.createForecast("TestNode", '1-06-2017', '1-07-2017', 'sales');
-        expect(result).to.be.a('Promise');
-        result.then((data) => {
-            expect(data.dataSetName).to.equal('TestNode');
-        }).then(done)
-            .catch((err) => { done(err); });
-    });
+    it('can create an forecast session', mochaAsync(async () => {
+        var result = await client.createForecast("TestNode", '1-06-2017', '1-07-2017', 'sales');
+        forecastSessionId = result.sessionId;
+
+        expect(result.dataSetName).to.equal('TestNode');
+    }));
+
+    it('can delete sessions', mochaAsync(async () => {
+        var result = await client.remove(forecastSessionId);
+
+        expect(result.status).to.equal(204);
+    }));
+
+    it('can get results', mochaAsync(async () => {
+        const value = await client.list();
+        var existing = null;
+        for (var index = 0; index < value.items.length; index++) {
+            var session = value.items[index];
+            if (session.status === "completed" && session.type === "forecast") {
+                existing = session;
+                break;
+            }
+        }
+        var session_result = await client.results(existing.sessionId);
+        expect(session_result.data).not.to.be.empty;
+    }));
 
     it('can get results by interval', mochaAsync(async () => {
         const value = await client.list();
@@ -68,5 +84,11 @@ describe('Session tests', () => {
         }
         const matrixResult = await client.confusionMatrixResults(existing.sessionId);
         expect(matrixResult.confusionMatrix).not.to.be.null;
+    }));
+
+    it('can list sessions using parameters', mochaAsync(async () => {
+        const result = await client.list('Location-A', 'event', '01-01-2017', '01-01-2100');
+
+        expect(result.items).not.to.be.empty;
     }));
 });
