e383a68f3d057490b6142a4755ea544c82089598
Craig Vermeer <craig@vermeers.net>
2018-01-29 17:07:01 -0500

    Use MochaAsync

--- tests/vocabulary.test.ts
+++ tests/vocabulary.test.ts
@@ -12,33 +12,33 @@ describe('Vocabulary tests', function () {
         expect(data.items.length).to.be.greaterThan(0);
     });
 
-    it('can query vocabularies by session id', async () => {
+    it('can query vocabularies by session id', mochaAsync(async () => {
         let data = await client.list({createdFromSession: "0160fb8f-de87-418c-8ef3-f1a479767437"});
         expect(data.items.length).to.be.greaterThan(0);
         for(var item of data.items) {
             expect(item.createdBySessionId).to.be.equal("0160fb8f-de87-418c-8ef3-f1a479767437")
         }
-    });
+    }));
 
-    it('can query vocabularies by data source', async () => {
+    it('can query vocabularies by data source', mochaAsync(async () => {
         let data = await client.list({dataSource: "Axon.Api.Verification.Regression_Airline"});
         expect(data.items.length).to.be.greaterThan(0);
         for(var item of data.items) {
             expect(item.dataSourceName).to.be.equal("Axon.Api.Verification.Regression_Airline")
         }
-    });
+    }));
 
-    it('can get words for a vocabulary', async () => {
+    it('can get words for a vocabulary', mochaAsync(async () => {
         let data = await client.get("9a6a4bf0-e85c-41bc-b3b9-76f7bba81f39");
         expect(data.id).to.be.equal("9a6a4bf0-e85c-41bc-b3b9-76f7bba81f39")
         expect(data.items.length).to.be.greaterThan(0);
-    });
+    }));
 
-    it('can filter words by type', async () => {
+    it('can filter words by type', mochaAsync(async () => {
         let data = await client.get("9a6a4bf0-e85c-41bc-b3b9-76f7bba81f39", {type: 'word'});
         expect(data.id).to.be.equal("9a6a4bf0-e85c-41bc-b3b9-76f7bba81f39")
         for(var word of data.items) {
             expect(word.type).to.be.equal("word")
         }
-    });
+    }));
 });
